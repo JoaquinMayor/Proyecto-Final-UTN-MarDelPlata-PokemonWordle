7729cdc3d096259d3f7c8b8e0e30c7d08701f0ba
ignacio tosini <ignaciotosini2002@gmail.com>
2023-10-08 13:42:07 -0300

    agregue el filtrado de la pokedex + tipografia

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -6,102 +6,82 @@ import 'tslib';
     styleUrls: ['./pokedex.component.scss']
 })
 export class PokedexComponent {
-    /*   let min = 1;
-      let max = 120;
-      
-      async function unPokemon(id) {
-          let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-          try {
-              const result = await fetch(url);
-              if (result.status === 200) {
-                  const json = await result.json();
-                  return json;
-              } else {
-                  alert("No se encontro el Digimon")
-              }
-          } catch (e) {
-              console.log(e);
-          }
-      }
-      
-      async function hacerLista() {
-          let pokemons = [];
-          let pokemon;
-          for (let i = min; i < max; i++) {
-              pokemon = await unPokemon(i);
-              pokemons.push(pokemon);
-          }
-      
-          return pokemons;
-      }
-      
-      async function tabla() {
-          let tabla = document.getElementById("lista");
-          let lista = [];
-          lista = await hacerLista();
-      
-          html = "";
-          for (element of lista) {
-              html += `<a href="./pokemon.html" onclick="verPokemon(${element.id})"><li class="pokemon">Nombre: ${element.name}
-              <img src="${element.sprites.front_default}" alt="Pokemon"></img></li><a>`;
-          }
-          tabla.innerHTML = html;
-      }
-      
-      // Función para filtrar los Pokémon según el texto ingresado
-      async function filtrarPokemons(texto) {
-          let pokemons = await hacerLista();
-          let listaFiltrada = [];
-      
-          let form = document.getElementById("containerForm");
-          let mensajeCantidad = form.querySelector("p");
-          let mensajeNoEncontrados = form.querySelector("p");
-      
-          if (texto.trim() !== "") {
-              listaFiltrada = pokemons.filter(pokemon =>
-                  pokemon.name.toLowerCase().includes(texto.toLowerCase())
-              );
-          } else {
-              listaFiltrada = pokemons;
-          }
-      
-          if (listaFiltrada.length === 0) {
-              if (mensajeNoEncontrados) {
-                  mensajeNoEncontrados = document.createElement("p");
-                  mensajeNoEncontrados.appendChild(document.createTextNode("No se encontraron coincidencias"));
-                  form.appendChild(mensajeNoEncontrados);
-                  form.removeChild(mensajeCantidad);
-              }
-          } else {
-              if (mensajeCantidad) {
-                  form.removeChild(mensajeCantidad);
-              }
-      
-              mensajeCantidad = document.createElement("p");
-              mensajeCantidad.appendChild(document.createTextNode("Cantidad de coincidencias: " + listaFiltrada.length));
-              form.appendChild(mensajeCantidad);
-          }
-      
-          mostrarPokemons(listaFiltrada);
-      }
-      
-      function mostrarPokemons(pokemons) {
-          let tabla = document.getElementById("lista");
-          let html = "";
-      
-          for (element of pokemons) {
-              //Se genera el HTML para cada Pokémon en la lista
-              html += `<a href="./pokemon.html" onclick="verPokemon(${element.id})"><li class="pokemon">Nombre: ${element.name}
-              <img src="${element.sprites.front_default}" alt="Pokemon"></img></li><a>`;
-          }
-          //Se actualiza el contenido del elemento <ul id="lista"></ul> en el DOM
-          tabla.innerHTML = html;
-      }
-      
-      function verPokemon(dato){
-          localStorage.setItem("dato", dato);
-      
-      }
-      
-      tabla(); */
+    min = 1;
+    max = 152;
+    pokemonList: any[] = [];
+    data: any;
+
+    ngOnInit() {
+        const storedData = localStorage.getItem("data");
+        if (storedData) {
+            this.data = JSON.parse(storedData);
+        }
+
+        this.renderTable();
+    }
+
+    async fetchPokemon(id: number) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        try {
+            const result = await fetch(url);
+            if (result.status === 200) {
+                const json = await result.json();
+                return json;
+            } else {
+                alert("No se encontró el Pokémon");
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async createPokemonList() {
+        const pokemons = [];
+        let pokemon;
+        for (let i = this.min; i < this.max; i++) {
+            pokemon = await this.fetchPokemon(i);
+            pokemons.push(pokemon);
+        }
+
+        return pokemons;
+    }
+
+    async renderTable() {
+        this.pokemonList = await this.createPokemonList();
+        this.showPokemons(this.pokemonList);
+    }
+
+    async filterPokemons(text: string) {
+        let filteredList = [];
+
+        if (text.trim() !== "") {
+            filteredList = this.pokemonList.filter((pokemon) =>
+                pokemon.name.toLowerCase().includes(text.toLowerCase())
+            );
+            console.log("entre")
+        } else {
+            filteredList = this.pokemonList;
+        }
+
+        this.showPokemons(filteredList);
+    }
+
+    showPokemons(pokemons: any) {
+        const table = document.getElementById("lista");
+        let html = "";
+
+        for (const element of pokemons) {
+            html += `<a href="./pokemon.html" onclick="this.viewPokemon(${element.id})"><li class="pokemon">Nombre: ${element.name}
+                <img src="${element.sprites.front_default}" alt="Pokemon"></img></li></a>`;
+        }
+
+        if (table) {
+            table.innerHTML = html;
+        }
+    }
+
+    viewPokemon(id: any) {
+        this.data = this.pokemonList.find(pokemon => pokemon.id === id);
+        localStorage.setItem("data", JSON.stringify(this.data));
+    }
 }
