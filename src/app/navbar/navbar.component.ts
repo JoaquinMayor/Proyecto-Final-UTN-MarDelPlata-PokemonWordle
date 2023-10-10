21d3751af3d6cb5a1cd3ff2696e51a4f85ba12ce
ignacio tosini <ignaciotosini2002@gmail.com>
2023-10-10 11:59:46 -0300

    menu desplegable + cositas de animacion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  dropdownActive: boolean = false;
 
+  toggleDropdown() {
+    this.dropdownActive = !this.dropdownActive;
+  }
 }
