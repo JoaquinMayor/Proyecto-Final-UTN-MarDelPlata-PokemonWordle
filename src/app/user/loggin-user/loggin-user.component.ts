e1991b422a54942ef8adcba06a46add7d7d7a066
ignacio tosini <ignaciotosini2002@gmail.com>
2023-10-13 01:03:54 -0300

    estilado loggin y userModify

--- src/app/user/loggin-user/loggin-user.component.ts
+++ src/app/user/loggin-user/loggin-user.component.ts
@@ -6,31 +6,29 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-loggin-user',
   templateUrl: './loggin-user.component.html',
-  styleUrls: ['./loggin-user.component.css']
+  styleUrls: ['./loggin-user.component.scss']
 })
-export class LogginUserComponent implements OnInit{
-  
-  name:string = "";
-  password:string = "";
-  log:boolean = false;
+export class LogginUserComponent implements OnInit {
+  name: string = "";
+  password: string = "";
+  log: boolean = false;
 
-  constructor(private userService:UsuariosServices, private router:Router){}
+  constructor(private userService: UsuariosServices, private router: Router) { }
   ngOnInit(): void {
     this.ifLogging()
   }
-  
+
   get user(): Usuario {
     return this.userService.user;
   }
-  
-  logging(){
+
+  logging() {
     this.userService.logging(this.name, this.password);
     this.router.navigate(["/home"]);
   }
-  ifLogging(){
-    if(this.userService.user.getId!=0){
+  ifLogging() {
+    if (this.userService.user.getId != 0) {
       this.router.navigate(["/home"]);
     }
-    
   }
 }
