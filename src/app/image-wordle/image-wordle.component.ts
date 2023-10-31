81e1a3f454dc6e8641c136819b8783dde1015f8e
ignacio tosini <ignaciotosini2002@gmail.com>
2023-10-31 00:09:55 -0300

    estilos wordle Imagenes

--- src/app/image-wordle/image-wordle.component.ts
+++ src/app/image-wordle/image-wordle.component.ts
@@ -9,26 +9,27 @@ import { UsuariosServices } from '../services/users.service';
   styleUrls: ['./image-wordle.component.scss']
 })
 export class ImageWordleComponent {
-  guessPokemon:Pokemon;
-  namePokemon:string = "";
-  generation:string ="";
-  idSelected:number =0;
-  show =false;
+  guessPokemon: Pokemon;
+  namePokemon: string = "";
+  generation: string = "";
+  idSelected: number = 0;
+  show = false;
   showButton: boolean = false;
-  showFinish:boolean = false;
+  showFinish: boolean = false;
   imgPokemon = "";
 
   pokemonList: Pokemon[] = [];
   filteredList: Pokemon[] = [];
   lives: number = 6;
   words: string[] = [];
-  guessed:string[] =[];
+  guessed: string[] = [];
 
-  imageScore:number = 0;
+  imageScore: number = 0;
 
-  constructor(private pokemonApiServices:PokemonApiServices,private userService:UsuariosServices){}
+  constructor(private pokemonApiServices: PokemonApiServices, private userService: UsuariosServices) { }
 
   generationSelected(generation: any) {
+    console.log("entre")
     this.generation = generation;
     this.renderTable();
   }
@@ -60,26 +61,24 @@ export class ImageWordleComponent {
   }
 
   showGame() {
-   this.userService.user.setTryImage(this.userService.user.getTryImage+1);
+    this.userService.user.setTryImage(this.userService.user.getTryImage + 1);
     this.startGame();
-    if(this.guessPokemon == undefined || this.idSelected == 0){
+    if (this.guessPokemon == undefined || this.idSelected == 0) {
       this.showButton = true;
     }
     this.show = true;
     this.lives = 6;
   }
 
-  startGame(){
-    
+  startGame() {
     let frontBack = Math.floor(Math.random() * 2);;
     this.generateRandomNumber();
     this.randomPokemon();
-    if(frontBack == 1){
+    if (frontBack == 1) {
       this.imgPokemon = this.guessPokemon.getBackSprite;
-    }else{
+    } else {
       this.imgPokemon = this.guessPokemon.getFrontSprite;
     }
-    
   }
 
   validationIfPokemon() {
@@ -92,32 +91,36 @@ export class ImageWordleComponent {
     return flag;
   }
 
-  searchPokemon(){
-    if(this.validationIfPokemon()){
-      if(this.guessPokemon.getName.toLowerCase() ==this.namePokemon.toLowerCase() && this.lives !=0){
+  searchPokemon() {
+    if (this.validationIfPokemon()) {
+      if (this.guessPokemon.getName.toLowerCase() == this.namePokemon.toLowerCase() && this.lives != 0) {
         this.imageScore++;
-        this.words.splice(0,this.words.length);
-        if(this.userService.user.getImageScore<this.imageScore){
+        this.words.splice(0, this.words.length);
+        if (this.userService.user.getImageScore < this.imageScore) {
           this.userService.user.setImageScore(this.imageScore);
-        } 
+        }
         this.guessed.push(this.guessPokemon.getFrontSprite);
         this.startGame();
-      }else if(this.guessPokemon.getName.toLowerCase() !=this.namePokemon.toLowerCase() && this.lives !=0){
+      } else if (this.guessPokemon.getName.toLowerCase() != this.namePokemon.toLowerCase() && this.lives != 0) {
         this.lives--;
         this.words.push(this.namePokemon);
         console.log(this.words)
-      }else if(this.lives == 0){
+      } else if (this.lives == 0) {
         this.showFinish = true;
-        this.words.splice(0,this.words.length);
+        this.words.splice(0, this.words.length);
         this.guessed.splice(0, this.guessed.length);
       }
     }
   }
 
-  finishGame(){
+  finishGame() {
     this.userService.user.setMaxScoreImage(this.userService.user.getMaxScoreImage + this.imageScore);
     this.show = false;
     this.showFinish = false;
+  }
+
+  selectPokemon(name: string) {
+    this.namePokemon = name.toLowerCase();
   }
 
 }
